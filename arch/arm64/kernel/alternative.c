AmpereComputing/ampere-centos-kernel---DEPRECATED

BACKPORT: VARIANT 2: arm64: Allow reference symbols in alt-seqs to be in kernel-data.
When patching-in alternative sequences during bootup, we copy
the alt-seq instructions to one or more target locations.
This relocation requires a fixup of references to symbols,
if they are outside of the alt-seq.

The decision if a fixup is required is implemented in the function
branch_insn_requires_update(), which checks if the given address
is in kernel-text (e.g. functions).

This patch renames the function to address_needs_relocation_fixup()
and also supports symbols in kernel-data (e.g. global variables).
This allows adrp to address such symbols, which is required for
e.g. ARM64_MISMATCHED_CACHE_LINE_SIZE.

This patch is part of the eMAG Spectre variant 2 patch set and required for
non-eMAG system.
This patch is backported from:
https://git.theobroma-systems.com/ampere-computing/linux.git/commit/?h=linux-4.16.12-amp&id=cd70cc560c99d814e0d10e1198cac7a91c124821

Change-Id: Ibb6538219747c2d96808992273cdc7e8b617fdff
Signed-off-by: Christoph Muellner <christoph.muellner@theobroma-systems.com>
Signed-off-by: Khuong Dinh <khuong.dinh@amperecomputing.com>

arch/arm64/kernel/alternative.c
@@ -38,17 +38,21 @@ struct alt_region {
 };
 
 /*
- * Check if the target PC is within an alternative block.
+ * Check if the given address needs a relocation fixup
+ * in case the given alt-seq is moved.
+ * This is required if the address is e.g. in kernel-text,
+ * but not if it is within the given alt-seq itself.
+ * We call BUG() in cases where we cannot safely decide.
  */
-static bool branch_insn_requires_update(struct alt_instr *alt, unsigned long pc)
+static bool address_needs_relocation_fixup(struct alt_instr *alt, unsigned long addr)
 {
 	unsigned long replptr;
 
-	if (kernel_text_address(pc))
+	if (kernel_text_address(addr) || core_kernel_data(addr))
 		return 1;
 
 	replptr = (unsigned long)ALT_REPL_PTR(alt);
-	if (pc >= replptr && pc <= (replptr + alt->alt_len))
+	if (addr >= replptr && addr <= (replptr + alt->alt_len))
 		return 0;
 
 	/*
@@ -77,7 +81,7 @@ static u32 get_alt_insn(struct alt_instr *alt, __le32 *insnptr, __le32 *altinsnp
 		 * do not rewrite the instruction, as it is already
 		 * correct. Otherwise, generate the new instruction.
 		 */
-		if (branch_insn_requires_update(alt, target)) {
+		if (address_needs_relocation_fixup(alt, target)) {
 			offset = target - (unsigned long)insnptr;
 			insn = aarch64_set_branch_offset(insn, offset);
 		}
@@ -88,7 +92,7 @@ static u32 get_alt_insn(struct alt_instr *alt, __le32 *insnptr, __le32 *altinsnp
 		orig_offset  = aarch64_insn_adrp_get_offset(insn);
 		target = align_down(altinsnptr, SZ_4K) + orig_offset;
 
-		if (branch_insn_requires_update(alt, target)) {
+		if (address_needs_relocation_fixup(alt, target)) {
 			/*
 			 * If we're replacing an adrp instruction, which uses
 			 * PC-relative immediate addressing, adjust the offset
@@ -104,7 +108,7 @@ static u32 get_alt_insn(struct alt_instr *alt, __le32 *insnptr, __le32 *altinsnp
 
 		target = (unsigned long)altinsnptr + offset;
 
-		if (branch_insn_requires_update(alt, target)) {
+		if (address_needs_relocation_fixup(alt, target)) {
 			/*
 			 * Disallow adr instructions for targets outside
 			 * of our alt block.

mlangsdorf/linux

arch/arm64/kernel/alternative.c
@@ -39,13 +39,17 @@ bool alternative_is_applied(u16 cpufeature)
 }
 
 /*
- * Check if the target PC is within an alternative block.
+ * Check if the given address needs a relocation fixup
+ * in case the given alt-seq is moved.
+ * This is required if the address is e.g. in kernel-text,
+ * but not if it is within the given alt-seq itself.
+ * We call BUG() in cases where we cannot safely decide.
  */
-static bool branch_insn_requires_update(struct alt_instr *alt, unsigned long pc)
+static bool address_needs_relocation_fixup(struct alt_instr *alt, unsigned long pc)
 {
 	unsigned long replptr;
 
-	if (kernel_text_address(pc))
+	if (kernel_text_address(pc) || core_kernel_data(pc))
 		return true;
 
 	replptr = (unsigned long)ALT_REPL_PTR(alt);
@@ -78,7 +82,7 @@ static u32 get_alt_insn(struct alt_instr *alt, __le32 *insnptr, __le32 *altinsnp
 		 * do not rewrite the instruction, as it is already
 		 * correct. Otherwise, generate the new instruction.
 		 */
-		if (branch_insn_requires_update(alt, target)) {
+		if (address_needs_relocation_fixup(alt, target)) {
 			offset = target - (unsigned long)insnptr;
 			insn = aarch64_set_branch_offset(insn, offset);
 		}
@@ -89,7 +93,7 @@ static u32 get_alt_insn(struct alt_instr *alt, __le32 *insnptr, __le32 *altinsnp
 		orig_offset  = aarch64_insn_adrp_get_offset(insn);
 		target = align_down(altinsnptr, SZ_4K) + orig_offset;
 
-		if (branch_insn_requires_update(alt, target)) {
+		if (address_needs_relocation_fixup(alt, target)) {
 			/*
 			 * If we're replacing an adrp instruction, which uses
 			 * PC-relative immediate addressing, adjust the offset
@@ -105,7 +109,7 @@ static u32 get_alt_insn(struct alt_instr *alt, __le32 *insnptr, __le32 *altinsnp
 
 		target = (unsigned long)altinsnptr + offset;
 
-		if (branch_insn_requires_update(alt, target)) {
+		if (address_needs_relocation_fixup(alt, target)) {
 			/*
 			 * Disallow adr instructions for targets outside
 			 * of our alt block.